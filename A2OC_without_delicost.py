06a1a3087dbacf248394baa24221ba0c480e6e0b
LihaoR <32670993+LihaoR@users.noreply.github.com>
2018-06-15 20:04:12 +0800

    Update A2OC_without_delicost.py

--- A2OC_without_delicost.py
+++ A2OC_without_delicost.py
@@ -128,9 +128,12 @@ class Worker():
         betas_omg      = betas_omg * (1-is_initial_betas)
         betas_OMG      = np.vstack(betas_omg)
         advantages_U   = discounted_rewards - q_options
-
         advantages_u   = np.zeros([batch_size])
-        advantages_OMG = q_options - np.max(q_options)
+        #advantages_OMG = q_options - np.max(q_options)
+        advantages_OMG = np.zeros([batch_size, num_options])
+        for i in range(batch_size):
+            advantages_OMG[i] = q_options[i] - np.max(q_options[i])
+        
         betas_omg         = np.zeros([batch_size])
         action            = np.zeros((batch_size, a_size))
         discounted_reward = np.zeros([batch_size])
